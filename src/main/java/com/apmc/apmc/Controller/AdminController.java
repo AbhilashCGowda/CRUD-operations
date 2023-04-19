da77e4a73e3fa9338569441b759688b29a2af3fb
AbhilashCGowda <abhilashc167@gmail.com>
2023-04-19 16:09:34 +0530

    Put and Delete operation

--- src/main/java/com/apmc/apmc/Controller/AdminController.java
+++ src/main/java/com/apmc/apmc/Controller/AdminController.java
@@ -3,6 +3,7 @@ package com.apmc.apmc.Controller;
 import com.apmc.apmc.Entity.Admin;
 import com.apmc.apmc.Service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,14 +16,39 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
 
+//    @PostMapping("/Admin/AddAdmin")
+//    public ResponseEntity<?> Admin(@RequestBody Admin admin){
+//        return adminService.AddAdmin(admin);
+//    }
+
+    //easier way
     @PostMapping("/Admin/AddAdmin")
-    public ResponseEntity<?> Admin(@RequestBody Admin admin){
-        return adminService.AddAdmin(admin);
+    public ResponseEntity<?> CreateAdmin(@RequestBody Admin admin){
+        return  new ResponseEntity<>(adminService.AddAdmin(admin), HttpStatus.OK);
     }
 
-    @GetMapping("/Admin/GetData")
-    public ResponseEntity<List<Admin>> GetAdmin(){
-        return adminService.GetAdmin();
+
+//    @GetMapping("/Admin/GetData")
+//    public ResponseEntity<List<Admin>> GetAdmin(){
+//        return adminService.GetAdmin();
+//    }
+
+    @GetMapping("/Admin/GetAdmin")
+    public ResponseEntity<?> GetAdmin(){
+        return  new ResponseEntity<>(adminService.GetAdmin(), HttpStatus.OK);
     }
 
+    @PutMapping("/Admin/UpdateAdmin/{userid}")
+    public ResponseEntity<?> EditUser(@RequestBody Admin obj, @PathVariable Integer userid) {
+        var result = adminService.UpdateAdmin(obj,userid);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+
+    //@DeleteMapping("/Admin/DeleteAdmin/{userid}")
+    @DeleteMapping("/Admin/DeleteAdmin/{userid}")
+    public ResponseEntity<String>DeleteAdmin(@PathVariable Integer userid)
+    {
+        return new ResponseEntity<>(adminService.DeleteAdmin(userid),HttpStatus.OK);
+    }
 }
--- src/main/java/com/apmc/apmc/Service/AdminService.java
+++ src/main/java/com/apmc/apmc/Service/AdminService.java
@@ -15,13 +15,45 @@ public class AdminService {
     @Autowired
     private AdminRepo adminRepo;
 
-    public ResponseEntity<String> AddAdmin(Admin admin){
+//    public ResponseEntity<String> AddAdmin(Admin admin){
+//        adminRepo.save(admin);
+//        return new ResponseEntity<>("Added Successfully", HttpStatus.CREATED);
+//    }
+
+    //can also do this way
+    public String AddAdmin(Admin admin){                                //to add
         adminRepo.save(admin);
-        return new ResponseEntity<>("Added Successfully", HttpStatus.CREATED);
+        return "Added Successfully";
     }
 
-    public ResponseEntity<List<Admin>> GetAdmin(){
-        var admin=adminRepo.findAll();
-        return new ResponseEntity<>(admin, HttpStatus.OK);
+//    public ResponseEntity<List<Admin>> GetAdmin(){
+//        var admin=adminRepo.findAll();
+//        return new ResponseEntity<>(admin, HttpStatus.OK);
+//    }
+
+    //can also do this way
+    public List<Admin> GetAdmin(){
+
+        return adminRepo.findAll();
     }
+
+    //if we use path variable in AdminController
+    public String UpdateAdmin(Admin obj, Integer userid){
+
+        var admin=adminRepo.findById(userid).orElseThrow(()-> new RuntimeException("Admin not found"));
+        admin.setName(obj.getName());
+        admin.setMobile(obj.getMobile());
+        admin.setPassword(obj.getPassword());
+        adminRepo.save(admin);
+        return  "Updated Successfully";
+    }
+
+    public String DeleteAdmin(Integer userid)
+    {
+        var admin =adminRepo.findById(userid).orElseThrow(()->new RuntimeException("User not found"));
+        adminRepo.delete(admin);
+        return "Deleted Successfully";
+    }
+
+
 }
